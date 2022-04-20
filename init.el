afc2af11b08feaeff17327e73cec8ad51691541b
kim <altroman@yeah.com>
2022-04-20 20:38:43 +0800

    resizing windows

--- init.el
+++ init.el
@@ -22,7 +22,10 @@
 ;; open shell the current window
 (add-to-list 'display-buffer-alist
              '("^\\*shell\\*$" . (display-buffer-same-window)))
-
+(global-set-key (kbd "<C-up>") 'shrink-window)
+(global-set-key (kbd "<C-down>") 'enlarge-window)
+(global-set-key (kbd "<C-left>") 'shrink-window-horizontally)
+(global-set-key (kbd "<C-right>") 'enlarge-window-horizontally)
 
 ;;Initialize package sources
 (require 'package)
